e024ea0f0bf4409006d475d01ec3d7a809d3a855
eranhaim <eranhaim321@gmail.com>
2021-09-15 11:32:35 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import img1 from "./imgs/1.jpeg";
+import img2 from "./imgs/2.jpeg";
+import img3 from "./imgs/3.jpeg";
+import img4 from "./imgs/4.jpeg";
+import img5 from "./imgs/5.jpeg";
 
-function App() {
+const day = window.location.href.split("/").slice(-1)[0];
+const answers = ["24752639", "88326016", "20111542", "100355140","238797120000"]
+const imgs = [img1,img2,img3,img4,img5]
+
+export default function App() {
+  const [ans, setAns] = useState("");
+  const [msg, setmsg] = useState("");
+  const [img, setImg] = useState("");
+
+  useEffect(() => {console.log(window.location.href.split("/"));setImg(imgs[day])},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", direction: "rtl", justifyContent: "center", alignItems: "center"}}>
+      <img style={{top:0,left:0,height: "100vh", width: "100vw", position: "fixed"}} src={img}/>
+      <input type="text" onChange={({target}) => setAns(target.value)}/>
+      <button onClick={() => {
+        if(ans == answers[day])
+          setmsg("כל הכבוד תותח");
+        else setmsg("אויש טעות");
+      }}>GO</button>
+      <div>
+      <h1>{msg}</h1></div>
     </div>
   );
 }
-
-export default App;
